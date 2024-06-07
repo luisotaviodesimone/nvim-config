f41347ad3b9001179dcecabd2e6acbc2e7f6463e
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-06-07 12:41:33 -0300

    feat: add sql formatter to conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,11 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { { "prettierd", "prettier" } },
+    sql = { "sql_formatter" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
