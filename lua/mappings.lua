703d2b049644af9be42ef9f8e0624916941903f9
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-06-14 23:06:00 -0300

    feat: add paste from system buffer mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,9 +9,15 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Makes it possible to copy to system buffer easier
-map("n", "<leader>y", "\"+y", { desc = "Copy to main buffer" })
-map("v", "<leader>y", "\"+y", { desc = "Copy to main buffer" })
-map("n", "<leader>Y", "\"+Y", { desc = "Copy to main buffer" })
+map("n", "<leader>y", "\"+y", { desc = "Copy to system buffer" })
+map("v", "<leader>y", "\"+y", { desc = "Copy to system buffer" })
+map("n", "<leader>Y", "\"+Y", { desc = "Copy to system buffer" })
+
+-- Makes it possible to paste from system buffer
+map("n", "<leader>p", "\"+p", { desc = "Paste from system buffer" })
+map("v", "<leader>p", "\"+p", { desc = "Paste from system buffer" })
+map("n", "<leader>P", "\"+P", { desc = "Paste from system buffer" })
+
 map('n', '<C-b>', ':NvimTreeToggle<CR>', {})
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
