d3bcbd2ccc8714287081d5da309035ded2b128e4
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-06-15 16:46:58 -0300

    feat: disable paste from system buffer in normal mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,9 +14,9 @@ map("v", "<leader>y", "\"+y", { desc = "Copy to system buffer" })
 map("n", "<leader>Y", "\"+Y", { desc = "Copy to system buffer" })
 
 -- Makes it possible to paste from system buffer
-map("n", "<leader>p", "\"+p", { desc = "Paste from system buffer" })
+-- map("n", "<leader>p", "\"+p", { desc = "Paste from system buffer" })
 map("v", "<leader>p", "\"+p", { desc = "Paste from system buffer" })
-map("n", "<leader>P", "\"+P", { desc = "Paste from system buffer" })
+-- map("n", "<leader>P", "\"+P", { desc = "Paste from system buffer" })
 
 map('n', '<C-b>', ':NvimTreeToggle<CR>', {})
 map("v", "J", ":m '>+1<CR>gv=gv")
