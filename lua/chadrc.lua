1820d46a61058b2f777aba01e8b7e421787fa94f
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-06-23 18:34:19 -0300

    refactor: formatting

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,28 +1,32 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "chadracula",
+  theme = "chadracula",
   transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+  group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
   pattern = "NvimTree_*",
   callback = function()
     local layout = vim.api.nvim_call_function("winlayout", {})
-    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
-  end
+    if
+      layout[1] == "leaf"
+      and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+      and layout[3] == nil
+    then
+      vim.cmd "confirm quit"
+    end
+  end,
 })
-
--- bananinha 12345
 
 return M
