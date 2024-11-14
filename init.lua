00434d3432ead145b5458b3517086b30e6972a71
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-11-14 16:15:06 -0300

    feat: add terraform commentstring

--- init.lua
+++ init.lua
@@ -43,3 +43,11 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
     vim.cmd "sleep 50m"
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
