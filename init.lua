e95476895f9d5ede60a2256051174c7d14c711cf
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-06-12 01:58:28 -0300

    feat: start nvchad migration

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require "options"
+
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
@@ -15,15 +17,15 @@ local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
-  },
+  -- {
+  --   "NvChad/NvChad",
+  --   lazy = false,
+  --   branch = "v2.5",
+  --   import = "nvchad.plugins",
+  --   config = function()
+  --     require "options"
+  --   end,
+  -- },
 
   { import = "plugins" },
 }, lazy_config)
@@ -32,7 +34,34 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "nvchad.autocmds"
+-- require "nvchad.autocmds"
+local autocmd = vim.api.nvim_create_autocmd
+
+-- user event that loads after UIEnter + only if file buf is there
+-- autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
+--   group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
+--   callback = function(args)
+--     local file = vim.api.nvim_buf_get_name(args.buf)
+--     local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+
+--     if not vim.g.ui_entered and args.event == "UIEnter" then
+--       vim.g.ui_entered = true
+--     end
+
+--     if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
+--       vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+--       vim.api.nvim_del_augroup_by_name "NvFilePost"
+
+--       vim.schedule(function()
+--         vim.api.nvim_exec_autocmds("FileType", {})
+
+--         if vim.g.editorconfig then
+--           require("editorconfig").config(args.buf)
+--         end
+--       end)
+--     end
+--   end,
+-- })
 
 vim.schedule(function()
   require "mappings"
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+-- require "nvchad.mappings"
 local builtin = require "telescope.builtin"
 local my_telescope = require "configs.telescope"
 local gitsigns = require "gitsigns"
@@ -52,8 +52,28 @@ map(
   { desc = "Copy repo url reference to clipboard" }
 )
 
+-- Remap jk to escape in insert mode
 map("i", "jk", "<ESC>")
 map("i", "JK", "<ESC>")
+
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
+
+map({ "n", "x" }, "<leader>fm", function()
+  require("conform").format { lsp_fallback = true }
+end, { desc = "general format file" })
+
+-- Navigation mappings
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 
 -- Makes it possible to copy to system buffer easier
 map("n", "<leader>y", '"+y', { desc = "Copy to system buffer" })
@@ -65,11 +85,20 @@ map("n", "<leader>Y", '"+Y', { desc = "Copy to system buffer" })
 map("v", "<leader>p", '"+p', { desc = "Paste from system buffer" })
 -- map("n", "<leader>P", "\"+P", { desc = "Paste from system buffer" })
 
--- LSP Diagnostics API mappings
+---- LSP
+-- Diagnostics API mappings
 map("n", "g]", ":lua vim.diagnostic.goto_next()<CR>", { desc = "LSP Go to next diagnostic" })
 map("n", "g[", ":lua vim.diagnostic.goto_prev()<CR>", { desc = "LSP Go to previous diagnostic" })
 map("n", "go", ":lua vim.diagnostic.open_float()<CR>", { desc = "LSP Open floating diagnostic message" })
 map("n", "gh", ":lua vim.lsp.buf.hover()<CR>", { desc = "LSP Open selected method description" })
+map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
+
+-- Comment
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
+-- terminal
+map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
 -- GitSigns mappings
 map("v", "<leader>gh", gitsigns.reset_hunk, { desc = "Reset selected git hunk" })
@@ -77,15 +106,11 @@ map("v", "<leader>gh", gitsigns.reset_hunk, { desc = "Reset selected git hunk" }
 -- UndoTree mappings
 map("n", "<leader>u", vim.cmd.UndotreeToggle)
 
--- NvimTree mappings
-map("n", "<C-b>", ":NvimTreeToggle<CR>", {})
-map("n", "<leader>co", ":NvimTreeCollapse<CR>", {})
-
--- Move line up and down
+-- Move selected line up and down
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
 
--- Remap page up and page down
+-- Remap page up and page down to centralize the cursor
 map("n", "<C-u>", "<C-u>zz")
 map("n", "<C-d>", "<C-d>zz")
 
@@ -93,8 +118,16 @@ map("n", "<C-d>", "<C-d>zz")
 map("n", "<leader>ff", my_telescope.my_find_files, { desc = "Telescope find files function" })
 map("n", "<leader>fr", builtin.registers)
 map("n", "<leader>fg", builtin.git_status)
-map("n", "<leader>fg", builtin.git_status)
 map("n", "<leader>fs", my_telescope.get_yaml_schemas(), { desc = "Activate new yaml schemas" })
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
 
 map("n", "<leader>tt", function()
   return require("base46").toggle_theme()
@@ -103,3 +136,7 @@ end, { desc = "Toggle Theme" })
 map("n", "<leader>tr", function()
   return require("base46").toggle_transparency()
 end, { desc = "Toggle Transperency" })
+
+map("n", "<leader>th", function()
+  require("nvchad.themes").open()
+end, { desc = "telescope nvchad themes" })
