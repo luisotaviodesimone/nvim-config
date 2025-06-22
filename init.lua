b8fb776a5ac378f253b6018ac53002f9b8f48f31
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-06-21 23:25:18 -0300

    feat: nvchad module enable

--- init.lua
+++ init.lua
@@ -17,15 +17,15 @@ local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-  -- {
-  --   "NvChad/NvChad",
-  --   lazy = false,
-  --   branch = "v2.5",
-  --   import = "nvchad.plugins",
-  --   config = function()
-  --     require "options"
-  --   end,
-  -- },
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
 
   { import = "plugins" },
 }, lazy_config)
