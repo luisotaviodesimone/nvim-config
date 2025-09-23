5cfd76291e3f44dd832e8abe2a63c646bf961e86
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-09-22 21:50:06 -0300

    feat: enable auto reload when file changed

--- lua/options.lua
+++ lua/options.lua
@@ -68,3 +68,9 @@ vim.filetype.add {
     ["helmfile.*%.ya?ml"] = "helm",
   },
 }
+
+-- Auto reload files changed outside of nvim
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  command = "if mode() != 'c' | checktime | endif",
+})
